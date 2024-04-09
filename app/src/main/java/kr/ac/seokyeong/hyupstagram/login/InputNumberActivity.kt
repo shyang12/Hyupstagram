6c5baec5560864f57166c688ee8699fe2807bc51
shyang12 <85710913+shyang12@users.noreply.github.com>
2024-04-09 21:11:01 +0900

    ID, PASSWORD 찾는 디비 설계 / 2024-04-09

--- app/src/main/java/kr/ac/seokyeong/hyupstagram/login/InputNumberActivity.kt
+++ app/src/main/java/kr/ac/seokyeong/hyupstagram/login/InputNumberActivity.kt
@@ -1,12 +1,30 @@
 package kr.ac.seokyeong.hyupstagram.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import kr.ac.seokyeong.hyupstagram.R
+import kr.ac.seokyeong.hyupstagram.databinding.ActivityInputNumberBinding
 
 class InputNumberActivity : AppCompatActivity() {
+    lateinit var binding : ActivityInputNumberBinding
+    val inputNumberViewModel : InputNumberViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_input_number)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_input_number)
+        binding.viewModel = inputNumberViewModel
+        binding.lifecycleOwner = this
+        setObserve()
+    }
+
+    fun setObserve() {
+        inputNumberViewModel.nextPage.observe(this) {
+            if(it) {
+                finish()
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+        }
     }
 }
